64d29f528b8ebe9bff66cebd93ca8aa4ee6227e2
Matthias Puhlmann <matthias.puhlmann@webit.de>
2022-08-08 16:51:43 +0200

    [TASK] add TYPO3 11.5 and PHP 8.1 to requirements

--- ext_localconf.php
+++ ext_localconf.php
@@ -6,6 +6,6 @@ if (!defined('TYPO3_MODE')) {
 // Register scheduler task
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['WebitDe\\DebugSettingsTask\\Task\\DebugSettingsTask'] = [
     'extension' => 'debug_settings_task',
-    'title' => 'LLL:EXT:' . 'debug_settings_task' . '/Resources/Private/Language/locallang_db.xml:debug_settings_task.title',
-    'description' => 'LLL:EXT:' . 'debug_settings_task' . '/Resources/Private/Language/locallang_db.xml:debug_settings_task.description',
+    'title' => 'LLL:EXT:debug_settings_task/Resources/Private/Language/locallang.xlf:task.debugSettingsTask.title',
+    'description' => 'LLL:EXT:debug_settings_task/Resources/Private/Language/locallang.xlf:task.debugSettingsTask.description',
 ];
